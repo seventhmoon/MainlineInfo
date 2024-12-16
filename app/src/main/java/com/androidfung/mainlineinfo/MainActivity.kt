ac6f6c11c21a7661ddd5720ac9707baa17776834
Fung Lam <chofung@google.com>
2024-12-16 14:14:58 +0000

    Layout Update

--- app/src/main/java/com/androidfung/mainlineinfo/MainActivity.kt
+++ app/src/main/java/com/androidfung/mainlineinfo/MainActivity.kt
@@ -9,8 +9,12 @@ import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
@@ -18,59 +22,78 @@ import com.androidfung.mainlineinfo.ui.theme.MainlineInfoTheme
 
 
 class MainActivity : ComponentActivity() {
+
+    private val packages = listOf("com.google.android.art")
+
+
+    @OptIn(ExperimentalMaterial3Api::class)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
             MainlineInfoTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = getMainlineArtworkVersion(this.baseContext).toString(),
-//                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
+                Scaffold(
+                    modifier = Modifier.fillMaxSize(),
+                    topBar = {
+                        TopAppBar(
+                            colors = TopAppBarDefaults.topAppBarColors(
+                                containerColor = MaterialTheme.colorScheme.primaryContainer,
+                                titleContentColor = MaterialTheme.colorScheme.primary,
+                            ),
+                            title = {
+                                Text("Mainline Info")
+                            }
+                        )
+                    },
+                ) { innerPadding ->
+                    packages.forEach {
+                        val versionCode = getMainlinePackageVersion(this.baseContext, it)
+                        val versionText =
+                            if (versionCode > 0) versionCode.toString()
+                            else "N/A"
+
+                        Text("$it : $versionText", Modifier.padding(innerPadding))
+                    }
                 }
             }
         }
     }
 }
 
-
-fun getMainlineArtworkVersion(context : Context) : Long {
+fun getMainlinePackageVersion(context: Context, packageName: String): Long {
 
     val result = StringBuilder()
 
-    var artVersionCode: Long = 0
+    var versionCode: Long = 0
 
 
     if (Build.VERSION.SDK_INT >= 31) {
         try {
-            artVersionCode = context.packageManager
-                .getPackageInfo("com.google.android.art", PackageManager.MATCH_APEX)
+            versionCode = context.packageManager
+                .getPackageInfo(packageName, PackageManager.MATCH_APEX)
                 .longVersionCode
         } catch (e: PackageManager.NameNotFoundException) {
             e.printStackTrace()
         }
+    }
+    return versionCode
+}
 
 
+fun getBaseArtVersion(context: Context): Long {
+    var artVersionCode: Long = 0
+
+    if (Build.VERSION.SDK_INT >= 31) {
         try {
-            if (artVersionCode == 0L) {
-                artVersionCode = context.getPackageManager()
-                    .getPackageInfo("com.android.art", PackageManager.MATCH_APEX)
-                    .longVersionCode
-            }
+            artVersionCode = context.packageManager
+                .getPackageInfo("com.android.art", PackageManager.MATCH_APEX)
+                .longVersionCode
         } catch (e: PackageManager.NameNotFoundException) {
             e.printStackTrace()
         }
     }
 
-
     return artVersionCode
-//    result.append("libart Version is : ").append(artVersionCode)
-
-//    return
-
-
 }
 
 @Composable
